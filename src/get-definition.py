f4e63bda95a2e9d745336a662850af21296fca1f
Angus Yuen <angusyuen@Anguss-MacBook-Pro.local>
2022-02-20 14:50:38 +1100

    fix: Fix handling words with no definitions

--- src/get-definition.py
+++ src/get-definition.py
@@ -31,6 +31,10 @@ def get_definition(traditional: str) -> Definition:
         KeyConditionExpression=Key('traditional').eq(traditional)
     )
 
+    if not response['Items']:
+        logger.debug(f"No definition found for {traditional}")
+        return None
+
     item = response['Items'][0]
     return Definition(
         traditional=item['traditional'],
@@ -56,7 +60,9 @@ def handler(event: typing.Dict, _: typing.Dict):
 
     try:
         definition = get_definition(event['queryStringParameters']['traditional'])
-        logger.debug(definition)
+        if not definition:
+            return _create_response(body=json.dumps({}))
+
         return _create_response(body=json.dumps(definition.__dict__))
     except ClientError as e:
         error_code = e.response['Error']['Code']
